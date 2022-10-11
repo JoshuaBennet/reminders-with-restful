c4ad6d3e855568fe8d71ea30fc532ff91d2a21b3
Josh Bennet <josh@thebennets.net>
2022-10-11 15:44:26 +0100

    trying to fix connection

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Navbar from "./Navbar";
 import RemindersList from "./RemindersList";
 
-function App() {
+const App = () => {
   return (
       <div className="App">
         <Navbar />
--- src/Reminder.js
+++ src/Reminder.js
@@ -1,29 +1,25 @@
-
-import {useState, useEffect} from 'react'
-import Reminder from "./Reminder";
-
-const RemindersList = () => {
-    const [reminders, setReminders] = useState([])
-
-    useEffect(() => {
-        fetch('http://localhost:4000/reminders')
-            .then((response) => {
-                return response.json()
-            }).then((responseBody) => {
-                const remindersFromApi = responseBody.data.reminders
-                setReminders(remindersFromApi)
-        })
-    }, [])
+// Reminder.js
+const Reminder = (props) => {
+    const handleClick = () => {
+        const requestOptions = {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                done: !props.done // set it to the opposite of the current value
+            })
+        }
+        const requestUrl = 'http://localhost:4000/reminders/' + props.id
+        fetch(requestUrl, requestOptions)
+    }
 
     return (
-        <div className={'remindersList'}>
-            {reminders.map((reminder) => <Reminder
-                id={reminder.id}
-                title={reminder.title}
-                done={reminder.done}
-            />)}
+        <div>
+            <input type="checkbox" onClick={handleClick} name={props.id} defaultChecked={props.done}/>
+            <label for={props.id}>{props.title}</label>
         </div>
     )
 }
 
-export default RemindersList
+export default Reminder
