3ee904f1e98dc9d8f5167bc775c6cf36bb8f4cdc
Josh Bennet <josh@thebennets.net>
2022-10-11 16:07:54 +0100

    routing for react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import Navbar from "./Navbar";
 import RemindersList from "./RemindersList";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Layout from "./Layout";
 
 const App = () => {
   return (
       <div className="App">
-        <Navbar />
+          <BrowserRouter>
+              <Routes>
+                  <Route path="/" element={<Layout />}>
+                      <Route index element={<Home />} />
+                      <Route path="about" element={<Add />} />
+                  </Route>
+              </Routes>
+          </BrowserRouter>
         <RemindersList />
       </div>
   );
